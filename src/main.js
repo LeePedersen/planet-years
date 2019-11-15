d43cf6114799b2507f1959ae2c7a6f25e075d065
Lee Pedersen <leepedersenr@gmail.com>
2019-11-15 15:15:32 -0800

    finish README

--- src/main.js
+++ src/main.js
@@ -5,5 +5,12 @@ import './styles.css';
 import { AgeExpect } from './ageExpect.js';
 
 $(document).ready(function() {
+  $("form#info").submit(function(event) {
+    let ageInput = $("#age").val();
+    let genderInput = $("select#gender").val();
+    let incomeInput = $("#income").val();
+
+    
+  });
 
 });
