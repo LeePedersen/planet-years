e368b5b19df0acf380d4ce15f16be400344af661
Lee Pedersen <leepedersenr@gmail.com>
2019-11-15 15:49:32 -0800

    complete UI logic

--- src/main.js
+++ src/main.js
@@ -6,11 +6,27 @@ import { AgeExpect } from './ageExpect.js';
 
 $(document).ready(function() {
   $("form#info").submit(function(event) {
+    event.preventDefault();
     let ageInput = $("#age").val();
     let genderInput = $("select#gender").val();
     let incomeInput = $("#income").val();
+    let mentalHealthInput = $("select#mentalHealth").val();
 
-    
+    let newAge = new AgeExpect(ageInput, mentalHealthInput, incomeInput, genderInput);
+
+    newAge.expectCalc();
+    newAge.mercury();
+    newAge.mars();
+    newAge.jupiter();
+    newAge.venus();
+
+    $("#mercury").text("Your age on Mercury: " + newAge.mercuryAge + " years");
+    $("#mercuryExp").text("Your life expectancy on Mercury: " + newAge.mercuryExpect + " more years");
+    $("#venus").text("Your age on Venus: " + newAge.venusAge + " years");
+    $("#venusExp").text("Your life expectancy on Venus: " + newAge.venusExpect + " more years");
+    $("#mars").text("Your age on Mars: " + newAge.marsAge + " years");
+    $("#marsExp").text("Your life expectancy on Mars: " + newAge.marsExpect + " more years");
+    $("#jupiter").text("Your age on Jupiter: " + newAge.jupiterAge + " years");
+    $("#jupiterExp").text("Your life expectancy on Jupiter: " + newAge.jupiterExpect + " more years");
   });
-
 });
