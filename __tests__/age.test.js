624b3ff48c73edd6c5391c2e02d6a0877cda98c1
Lee Pedersen <leepedersenr@gmail.com>
2019-11-15 09:31:11 -0800

    create rest of tests for planet years

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -12,4 +12,19 @@ describe('Age', () => {
     newAge.mercury();
     expect(newAge.mercuryAge).toEqual("104.17");
   });
+
+  test('should check if venus prototype returns age on venus', () => {
+    newAge.venus();
+    expect(newAge.venusAge).toEqual("40.32");
+  });
+
+  test('should check if mars prototype returns age on mars', () => {
+    newAge.mars();
+    expect(newAge.marsAge).toEqual("13.30");
+  });
+
+  test('should check if jupiter prototype returns age on jupiter', () => {
+    newAge.jupiter();
+    expect(newAge.jupiterAge).toEqual("2.11");
+  });
 });
